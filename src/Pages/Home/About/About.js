26397ebe591f253c74a5b615aaf16abf7eab9443
faisal-al-fatha <faysal313@gmail.com>
2022-11-25 05:09:06 +0600

    about section

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,78 @@
+
+const About = () => {
+    return (
+        <section className="text-gray-900">
+	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+		<div>
+			<h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl ">What is Bike Paradise</h2>
+			<p className="max-w-3xl mx-auto mt-4 text-xl text-center">Bike Paradise is a search engine that locates motorcycle listings <br /> posted on the web, and makes them all available to you through a simple and fast discovery experience. </p>
+		</div>
+		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+			<div>
+				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl ">Our Features</h3>
+				<p className="mt-3 text-lg ">Read on to find out how Bike Paradise can help you find your next motorcycle.</p>
+				<div className="mt-12 space-y-12">
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md bg-emerald-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 ">Biggest selection</h4>
+							<p className="mt-2 ">Our search technology allows us to locate as many listings as possible, from anywhere they appear on the internet. This results in the biggest selection of motorcycles available for sale online. Go ahead, try it out!</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md bg-emerald-400 ">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 ">Make an informed decision</h4>
+							<p className="mt-2 ">We give you additional information about each listing to help you understand if that's the motorcycle for you. We calculate average prices so you know if you are getting a good deal, and we show you how each model's key specs differs from its competition.</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md bg-emerald-400 text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 ">Fastest online experience</h4>
+							<p className="mt-2 ">You wouldn't want your motorcycle to be slow, so why settle for slow-loading sites? MotoHunt is the fastest shopping experience online.</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md bg-emerald-400 text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 ">Make your wishlist</h4>
+							<p className="mt-2 ">With so many motorcycles to chose from, one can get lost. Save the listings you like into your own private wishlist</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div aria-hidden="true" className="mt-10 lg:mt-0">
+				<img src="https://images.unsplash.com/photo-1591637333184-19aa84b3e01f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" className="mx-auto rounded-lg shadow-lg" />
+			</div>
+		</div>
+	</div>
+</section>
+    );
+};
+
+export default About;
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <section className="bg-gray-300 text-black">
+    <section className="text-black">
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row">
         <div className="carousel w-full lg:w-1/2 h-[422]">
           <div id="slide1" className="carousel-item relative w-full">
             <img
               src="https://images.unsplash.com/photo-1614825907934-63c12063709e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1133&q=80"
               alt=""
-              className="w-full"
+              className="w-full rounded-lg"
             />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide3" className="btn btn-circle">
@@ -24,7 +24,7 @@ const Banner = () => {
             <img
               src="https://images.unsplash.com/photo-1589122350591-964f4987a296?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               alt=""
-              className="w-full"
+              className="w-full rounded-lg"
             />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide1" className="btn btn-circle">
@@ -39,7 +39,7 @@ const Banner = () => {
             <img
               src="https://images.unsplash.com/photo-1608461688104-304d5fe60da1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               alt=""
-              className="w-full"
+              className="w-full rounded-lg"
             />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide2" className="btn btn-circle">
