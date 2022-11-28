b4e1317e521465d203407632d1ded1f6af588f8f
faisal-al-fatha <faysal313@gmail.com>
2022-11-28 15:40:51 +0600

    booking shown in table

--- src/Pages/Dashboard/MyBookings/MyBookings.js
+++ src/Pages/Dashboard/MyBookings/MyBookings.js
@@ -1,17 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
 import { useContext } from 'react';
 import { AuthContext } from '../../../Context/Authprovider';
 
 const MyBookings = () => {
-    const user = useContext(AuthContext)
+    const user = useContext(AuthContext);
+    const {data: bookings = []}= useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async() => {
+            const res = await fetch(`http://localhost:5000/bookings?email=${user.user.email}`);
+            const data = await res.json();
+            return data;
+        }
+
+    })
     return (
         <div>
             <div className='text-center'>
             <h3 className='text-2xl font-bold my-6'> All Bookings of {user.user.displayName} </h3>
             </div>
             <div className="overflow-x-auto">
-  <table className="table table-zebra w-full">
+  <table className="table w-full">
     
-    <thead className='bg-slate-300'>
+    <thead>
       <tr >
         <th></th>
         <th>Name</th>
@@ -21,12 +31,16 @@ const MyBookings = () => {
     </thead>
     <tbody>
      
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td><button className='btn-secondary px-4 py-1 rounded'>Buy Now</button></td>
-      </tr>
+     {
+        bookings.map((booking, i) => 
+            <tr key={booking._id}>
+            <th>{i + 1}</th>
+            <td>{booking.productName}</td>
+            <td>${booking.price}</td>
+            <td><button className='btn-secondary px-4 py-1 rounded'>Buy Now</button></td>
+          </tr>
+        )
+     }
      
 
     </tbody>
